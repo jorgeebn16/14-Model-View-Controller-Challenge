55dfea0b936179b36fc7bea7c1093e56639a01d8
jorgeebn16 <jorgeebn16@gmail.com>
2021-02-11 21:03:28 -0500

    Final Update

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,7 +5,7 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && password) {
+    if (username && password.value>=8) {
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({
@@ -20,9 +20,10 @@ async function signupFormHandler(event) {
 
             document.location.replace('/dashboard');
 
-        } else {
+        } else { if (password.value<8) {
             //alert(response.statusText);
             alert("Password do not meet minimun requirements of 8 Characters. Please try using a new Password");
+        }
         }
     }
 }
